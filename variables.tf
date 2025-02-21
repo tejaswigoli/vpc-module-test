ccbcf83a4406dbc1cdbb50a0b73e4b4f001b0066
Tejaswi Goli <tejaswigoli@gmail.com>
2025-02-21 21:02:41 +0000

    module

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    default = "expense"
+}
+variable "environment" {
+    default = "dev"
+}
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        Terraform = "true"
+    }
+  
+}
---
+++ vpc.tf
@@ -0,0 +1,3 @@
+module "vpc" {
+    source = "../terraform-aws-vpc"
+}
