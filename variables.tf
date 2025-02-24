a6eb953610e5b7f07f039343ebcffcdcb3a8fdc2
Tejaswi Goli <tejaswigoli@gmail.com>
2025-02-24 14:44:32 +0000

    vpc

--- variables.tf
+++ variables.tf
@@ -13,5 +13,19 @@ variable "common_tags" {
         environment = "dev"
         Terraform = "true"
     }
-  
+}
+variable "vpc_tags" {
+  default = {
+    Purpose = "assignment"
+  }
+}
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+variable "database_subnet_cidrs" {
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
